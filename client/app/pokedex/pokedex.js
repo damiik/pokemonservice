ca4c6d2841c263b54a3d4d01fd9de162d9d52f27
Dariusz Mikołajczyk <dariusz.mikolajczyk@partnertech.pl>
2016-01-31 20:38:44 +0100

    added: pokedex.controller.js, pokedex.js, pokedex.add.html, pokedex.view.html

--- client/app/pokedex/pokedex.js
+++ client/app/pokedex/pokedex.js
@@ -4,8 +4,22 @@ angular.module('angularfull2App')
   .config(function ($stateProvider) {
     $stateProvider
       .state('pokedex', {
-        url: '/pokedex',
+        url: '/',
         templateUrl: 'app/pokedex/pokedex.html',
         controller: 'PokedexCtrl'
+      })
+      .state('pokedex.add', {
+      	url: 'add',
+      	controller: 'PokedexCtrl',
+      	views: {
+        	'pokedex-content': { templateUrl: 'app/pokedex/pokedex.add.html' }
+        }
+      })
+      .state('pokedex.view', {
+      	url: ':name',
+      	controller: 'PokedexCtrl',
+      	views: {
+      		'pokedex-content': { templateUrl: 'app/pokedex/pokedex.view.html' }
+      	}
       });
   });
